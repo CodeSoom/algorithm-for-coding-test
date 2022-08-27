6ba17e18b303f285f171b1902a3bcb80da90e287
kyuwon53 <lkw2v@naver.com>
2022-08-27 23:17:01 +0900

    DAY02 문자열 폭발 풀이

---
+++ DAY-02/kyuwon53/test_solution9935.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from solution9935 import solution
+
+
+class Test(TestCase):
+    def test_solution(self):
+        sentence = 'mirkovC4nizCC44'
+        bomb = 'C4'
+        result = 'mirkovniz'
+        self.assertEqual(solution(sentence, bomb), result)
+    def test_solution_all(self):
+        sentence = '12ab112ab2ab'
+        bomb = '12ab'
+        result = 'FRULA'
+        self.assertEqual(solution(sentence, bomb), result)
